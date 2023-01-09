94d791555abfe24d7400a0de143198931e4f6033
Camila Barpp <milabarpp5.com>
2023-01-09 14:50:41 -0300

    Agora o background do header muda de cor quando scrolla

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  OnInit,
+  HostListener,
+} from '@angular/core';
 
 @Component({
   selector: 'header',
@@ -9,14 +16,30 @@ export class HeaderComponent implements OnInit {
   showSideNav!: boolean;
   @Input() isLoggedIn!: boolean;
   @Output() toggledSideNav: EventEmitter<boolean> = new EventEmitter();
-  background: any;
 
-  ngOnInit(): void {}
+  @Input() logo?: string;
+  @Input() menu?: string;
+  scrolled: boolean = false;
+
+  ngOnInit(): void {
+    this.onScroll();
+  }
 
   toggleSideNav() {
     this.toggledSideNav.emit();
     this.showSideNav = !this.showSideNav;
   }
 
-
+  @HostListener('window:scroll', [])
+  onScroll() {
+    if (window.pageYOffset > 0) {
+      this.scrolled = true;
+      this.logo = 'assets/logo.svg';
+      this.menu = 'assets/menu2.svg';
+    } else {
+      this.logo = 'assets/logo2.svg';
+      this.menu = 'assets/menu.svg';
+      this.scrolled = false;
+    }
+  }
 }
